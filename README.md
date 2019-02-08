16ac99b5ca2bbdff509a03a4e7d3d5722eeb908e
Robbie Martinus <12573669+rmartinus@users.noreply.github.com>
2019-02-09 07:50:44 +1100

    Use StreamListener annotation to handle messages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-cloud-stream-rabbit-consumer
 Consumer for spring-cloud-stream-rabbit
+See: https://github.com/rmartinus/spring-cloud-stream-rabbit for instructions
--- src/main/java/com/cloud/stream/rabbit/cloudstreamrabbit/cloudstreamrabbit/CloudStreamRabbitConsumerApplication.java
+++ src/main/java/com/cloud/stream/rabbit/cloudstreamrabbit/cloudstreamrabbit/CloudStreamRabbitConsumerApplication.java
@@ -6,41 +6,25 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.messaging.SubscribableChannel;
+import org.springframework.cloud.stream.annotation.StreamListener;
 
 @SpringBootApplication
 @EnableBinding(ConsumerChannels.class)
 public class CloudStreamRabbitConsumerApplication {
 
+    private static final Log log = LogFactory.getLog(CloudStreamRabbitConsumerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CloudStreamRabbitConsumerApplication.class, args);
     }
 
-    private IntegrationFlow incomingMessageFlow(SubscribableChannel incoming, String prefix) {
-        Log log = LogFactory.getLog(getClass());
-        return IntegrationFlows
-                .from(incoming)
-                .transform(String.class, String::toUpperCase)
-                .handle(String.class,
-                        (greeting, headers) -> {
-                            log.info("greeting received in IntegrationFlow (" + prefix + "): " + greeting);
-                            return null;
-                        }).get();
+    @StreamListener(ConsumerChannels.DIRECTED)
+    public void handleDirected(String msg) {
+        log.info("Directed message: " + msg);
     }
 
-    @Bean
-    @Primary
-    IntegrationFlow direct(ConsumerChannels channels) {
-        return incomingMessageFlow(channels.directed(), "directed");
-    }
-
-    @Bean
-    IntegrationFlow broadcast(ConsumerChannels channels) {
-        return incomingMessageFlow(channels.broadcasts(), "broadcasts");
+    @StreamListener(ConsumerChannels.BROADCASTS)
+    public void handleBroadcast(String msg) {
+        log.info("Broadcast message: " + msg);
     }
 }
-
